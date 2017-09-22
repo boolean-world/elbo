07013d719d8eaf79d03b75833a851510b13987d2
Supriyo Biswas <supriyo.biswas.96@gmail.com>
2017-09-23 02:45:33 +0530

    Allow a trailing slash at the end of the path.

--- public/index.php
+++ public/index.php
@@ -8,6 +8,10 @@ if (preg_match('#^/assets/#', $request_uri) === 1) {
 	return false;
 }
 
+if ($request_uri !== "/" && $request_uri[-1] === "/") {
+	$request_uri = substr($request_uri, 0, -1);
+}
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $container = require __DIR__.'/../app/services.php';
