d16df6ebf8bc02836c2925d9b6ba819b30a43328
Supriyo Biswas <supriyo.biswas.96@gmail.com>
2017-10-12 14:57:25 +0530

    Check the deny_regex value when redirecting.

--- app/Middlewares/ShortURLVerified.php
+++ app/Middlewares/ShortURLVerified.php
@@ -2,7 +2,7 @@
 
 namespace Elbo\Middlewares;
 
-use Elbo\Library\URL;
+use Elbo\Library\{URL, Configuration};
 use Elbo\Models\{User, ShortURL, DomainPolicy};
 use Symfony\Component\HttpFoundation\{Request, Response};
 
@@ -25,6 +25,21 @@ trait ShortURLVerified {
 			return new Response($twig->render($this->warningPage ?? 'disabled.html.twig', [
 				'login_email' => $email
 			]), 403);
+		}
+
+		$config = $this->container->get(Configuration::class);
+		$deny_regex = $config->get('url_policies.deny_urls', null);
+
+		if ($deny_regex !== null && $deny_regex !== "") {
+			$deny_regex = '/'.str_replace('/', '\/', $deny_regex).'/';
+
+			if (preg_match($deny_regex, $res->url)) {
+				$twig = $this->container->get(\Twig_Environment::class);
+
+				return new Response($twig->render($this->warningPage ?? 'disabled.html.twig', [
+					'login_email' => $email
+				]), 403);
+			}
 		}
 
 		$policy = DomainPolicy::getPolicy(URL::determineHostName($res->url));
