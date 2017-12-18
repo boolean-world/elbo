c36e5be97869170f64d254e1a6f9cad301e22813
Supriyo Biswas <supriyo.biswas.96@gmail.com>
2017-12-18 16:40:11 +0530

    Show user email when access is denied.

--- app/Middlewares/RedirectIfNotAdmin.php
+++ app/Middlewares/RedirectIfNotAdmin.php
@@ -8,12 +8,12 @@ use Symfony\Component\HttpFoundation\{Request, Response, RedirectResponse};
 trait RedirectIfNotAdmin {
 	protected function redirectIfNotAdmin(Request $request) {
 		$id = $this->session->get('userid');
-		$user = User::where('id', $id)->where('disabled', '<>', 1)->select('admin')->first();
+		$user = User::where('id', $id)->select('admin', 'disabled', 'email')->first();
 
 		if ($user === null) {
 			return new RedirectResponse('/~login?redirect='.urlencode($request->getPathInfo()));
 		}
-		else if ($user->admin !== 1) {
+		else if (!$user->admin || $user->disabled) {
 			$twig = $this->container->get(\Twig_Environment::class);
 
 			return new Response($twig->render('errors/notfound.html.twig', [
