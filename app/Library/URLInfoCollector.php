eade2b3f071ed04249a9f4f989c3037da81df917
Supriyo Biswas <supriyo.biswas.96@gmail.com>
2018-08-07 10:16:59 +0530

    Update whitelist.

--- app/Library/URLInfoCollector.php
+++ app/Library/URLInfoCollector.php
@@ -81,7 +81,7 @@ class URLInfoCollector {
 
 	protected static function getClientRedirect(string $str) {
 		if (preg_match('/<meta http-equiv=[^>]+ content=[^>]+url=([^"\'>]+)/i', $str, $matches)) {
-			if (preg_match('/no(?:js|javascript)/', $matches[1])) {
+			if (preg_match('/no(?:js|(?:java)?script)/', $matches[1])) {
 				return null;
 			}
 
